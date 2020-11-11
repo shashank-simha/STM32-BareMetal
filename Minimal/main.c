2ff3027e01f9fb83e38180ec2b220d31a0e04c15
shashank-simha <shashanksimha183@gmail.com>
2020-11-11 19:03:24 +0530

    Add codes | Compile without errors

---
+++ Minimal/main.c
@@ -0,0 +1,18 @@
+/**
+ * Main program.
+ */
+
+void SystemInit(void)
+{
+
+}
+
+int main(void) 
+{
+  int val = 0;
+  // To test RAM '.data' section initialization:
+  //static int dont_panic = 42;
+  while (1) {
+    val += 1;
+  }
+}
