8b657e490507673751883f115ac82f4b0adf88a5
shashank-simha <shashanksimha183@gmail.com>
2021-02-15 22:14:17 +0530

    Rename Blink to Button Blink

---
+++ Button-Blink/Src/main.h
@@ -0,0 +1,12 @@
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#include <stdint.h>
+#include "stm32f446xx.h"
+
+#define B1_Pin 13
+#define B1_GPIO_Port GPIOC
+#define LD2_Pin 5
+#define LD2_GPIO_Port GPIOA
+
+#endif
